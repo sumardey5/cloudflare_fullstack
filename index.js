20ab623b24ca24d0529e8a6f829b2698d676334e
sumardey5 <sumardey5@gmail.com>
2020-04-19 03:44:48 -0500

    Achieved MVP

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  try {
+    const urls = await fetch('https://cfw-takehome.developers.workers.dev/api/variants')
+    .then((response) => {
+      return response.json();
+    });
+    const newURL = await fetch(urls['variants'][Math.floor(Math.random() * 2)])
+    .then((response) => {
+      return response.text();
+    });
+    return new Response(newURL, {headers: { 'content-type': 'text/html' },});
+  }
+  catch{
+    window.location.href = "http://www.w3schools.com"
+  }
 }
